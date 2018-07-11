817c943390eaf58dea3110cecc7af68cb7c85ab6
hb_java@sina.com <Java19786028>
2018-07-11 14:47:32 +0800

    shrink

--- prod/aggressive.py
+++ prod/aggressive.py
@@ -5,6 +5,7 @@ import codedata
 import json
 import time
 import pandas
+import logging
 
 highcodes = [];
 lowcodes = [];
@@ -14,13 +15,13 @@ lowtotal = 0
 # pe41 = ['600004','600000']
 
 basics = pandas.read_json('df.json',orient='table')
-basics = basics[basics['pe'] < 41][basics['pe'] > 0][basics['pb'] < 5][basics['pb'] > 0][basics['rev'] > 0][basics['profit'] > 0][basics['industry'] != '区域地产'][basics['industry'] != '全国地产'][basics['industry'] != '银行']
+basics = basics[basics['pe'] < 21][basics['pe'] > 20.8][basics['pb'] < 5][basics['pb'] > 0][basics['rev'] > 0][basics['profit'] > 0][basics['industry'] != '区域地产'][basics['industry'] != '全国地产'][basics['industry'] != '银行']
 pe41 = basics.index
-# print(basics.index)
+print(basics.index)
 
 for code in pe41:
-  code = str(code)
-  print(code)
+  code = "%06d" % code
+  logging.warning(code)
   df = ts.get_k_data(code, start='1990-12-01')
   if df.shape[0]>30:
     count += 1
@@ -33,7 +34,8 @@ for code in pe41:
     low = lowfilter[lowfilter[lowfilter == False][-1:].index[0] + 1:-1].shape[0]
     lowtotal += low
     lowcodes.append([low, code])
-    print(code)
+    logging.warning(highcodes)
+    logging.warning(lowcodes)
 
 highcodes.sort(reverse=True)
 subject = "High:%f, Low:%f, Date: %s" % (round(hightotal / count, 2), round(lowtotal / count, 2), time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
--- prod/test-prod.py
+++ prod/test-prod.py
@@ -4,17 +4,19 @@ import smail
 import codedata
 import json
 import time
-from pandas import *
+import pandas
 
-# df = ts.get_stock_basics()
-# df.to_json('df.json',orient='table')
-# DataFrame.to_json()
-#
-# # basics.to_json('/home/ubuntu/workspace/pstudy/prod/basics.json',orient='records')
-#
+highcodes = [];
+lowcodes = [];
+count = 0
+hightotal = 0
+lowtotal = 0
+# pe41 = ['600004','600000']
+
 basics = pandas.read_json('df.json',orient='table')
-basics = basics[basics['pe'] < 41][basics['pe'] > 0][basics['pb'] < 5][basics['pb'] > 0][basics['rev'] > 0][basics['profit'] > 0][basics['industry'] != '区域地产'][basics['industry'] != '全国地产'][basics['industry'] != '银行']
-print(basics.index)
-
-# df = pandas.read_json('d:/workspace/pstudy/prod/df.json',orient='records');
-# print(df.type)
+print(basics)
+# print(basics[basics['pe'] < 41])
+#basics = basics[basics['pe'] < 41][basics['pe'] > 0][basics['pb'] < 5][basics['pb'] > 0][basics['rev'] > 0][basics['profit'] > 0][basics['industry'] != '区域地产'][basics['industry'] != '全国地产'][basics['industry'] != '银行']
+#pe41 = basics.index
+# print(basics.index)
+# # basics.to_json('/home/ubuntu/workspace/pstudy/prod/basics.json',orient='records')
